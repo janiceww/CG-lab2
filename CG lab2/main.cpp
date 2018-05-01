efaccccef4da6c76c2f35d78f56e8789eea6a569
Janice <janicewang.127@gmail.com>
2018-05-01 15:02:11 -0400

    Initial Commit

---
+++ CG lab2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CG lab2
+//
+//  Created by Janice Wang on 5/1/18.
+//  Copyright © 2018 Janice Wang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
